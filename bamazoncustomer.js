de285a88436c51ed3828c6218fad9c1ac8d0c46a
alexdecycive <alex@decycive.com>
2017-11-09 07:58:50 -0500

    Updates to packages and updates to code - working up to ability to check sql for user inputs

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -7,6 +7,11 @@ console.log("-------------------------------____________________----------------
 
 
 var mysql = require('mysql');
+var prompt = require("prompt");
+var delayed = require("delayed");
+var inquirer = require("inquirer");
+
+
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -15,13 +20,52 @@ var connection = mysql.createConnection({
   database : 'bamazon'
 });
 
+
 var products = [];
+var itemID = 0;
+var itemquant = 0;
+var questionsOFF =0;
+var itemselected = [];
+var prod_quant = 0;
 
 
+var questions = [{
+  name: 'itemIDask',
+  message: 'What is the product ID you want to order?'
+}, {
+  name:'itemquant',
+  message: "What quantity do you want to order?",
+}]
 
-//Start the store
+
+//Run the store
 
 displayitems();
+
+//delayed.delay(function () { itemtobuy() }, 1000);
+//delayed.delay(function () { getquantity() }, 1000);
+
+delayed.delay(function () { 
+
+inquirer.prompt(questions).then(function(answers){
+ console.log("YOUR ANSWERS:" + answers.itemIDask + ' and ' + answers.itemquant); //an object containing the user response.
+ itemID = answers.itemIDask;
+ itemquant = answers.itemquant;
+ questionsOFF = 1;
+ console.log("GET OFF: " + questionsOFF);
+ checksql();
+
+});
+
+}, 2000);
+
+checksql();
+
+
+
+if (questionsOFF === 1) {
+	console.log("YOUR ANSWERS GLOBAL:" + itemID + ' and ' + itemquant); //an object containing the user response.
+}
 
 
 
@@ -32,6 +76,9 @@ function displayitems () {
 	console.log(" ");
 	console.log("Welcome to the store.")
 	get_products();
+
+	// DELAY TO ENSURE TABLE SHOWS BEFORE PROMPT: print "beep" to the console after 1/2 a second 
+//delayed.delay(function () { console.log('beep') }, 1000)
 
 //Close the displayitems function
 }
@@ -73,6 +120,62 @@ function get_products() {
 	});
 
 
-	connection.end();
+	
 }
 
+
+
+
+function itemtobuy() {
+
+console.log("Please tell me what Product ID you want to buy: ");
+prompt.get(['ID'], function (err, result) {
+  // Log the results. 
+  console.log(' --> You chose: ' + result.ID);
+  var itemID = result.ID
+  //console.log("Item ID is: " + itemID);
+  if (itemID != 0) {
+	getquantity()
+}
+
+
+//Closing the prompt
+ });
+
+//Closing the function
+}
+
+function getquantity() {
+
+console.log("Please tell me what quantity you need: ");
+prompt.get(['quant'], function (err, result) {
+  // Log the results. 
+  console.log(' --> You chose: ' + result.quant);
+
+
+  var itemquant = result.quant
+  console.log("Item ID is: " + itemID);
+
+
+//Closing the prompt
+ });
+
+//Closing the function
+}
+
+
+function checksql() {
+	 
+	connection.query('SELECT * FROM products WHERE item_ID=' + itemID, function (error, results, fields) {
+	  if (error) throw error;
+
+	  itemselected = results;
+	  prod_quant = results.stock_quantity;
+	  console.log("SQL RESULTS: " + itemselected + " AND RESULTS QUANT: " + prod_quant);
+	  
+
+	});
+
+
+	connection.end();
+}
